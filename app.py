98929b4ee27e16e723bccc8d74aae5dc21c2f7ca
Hridhya Paul <119098652+Hridhyapaul3@users.noreply.github.com>
2026-04-08 14:51:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -361,5 +361,8 @@ if __name__ == "__main__":
         db.create_all()
     app.run(debug=True)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
